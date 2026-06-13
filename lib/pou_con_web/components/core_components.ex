6c6775116d3d22728b140f86e5ecfe204d3c5b09
Tan Kwang How <tankwanghow@gmail.com>
2026-06-13 12:11:09 +0800

    Allow type attribute on button component.
    Forward the HTML type attribute for form submit and reset buttons.

--- lib/pou_con_web/components/core_components.ex
+++ lib/pou_con_web/components/core_components.ex
@@ -87,7 +87,7 @@ defmodule PouConWeb.CoreComponents do
       <.button phx-click="go" variant="primary">Send!</.button>
       <.button navigate={~p"/"}>Home</.button>
   """
-  attr :rest, :global, include: ~w(href navigate patch method download name value disabled)
+  attr :rest, :global, include: ~w(href navigate patch method download name value disabled type)
   attr :class, :string
   attr :variant, :string, values: ~w(primary)
   slot :inner_block, required: true
